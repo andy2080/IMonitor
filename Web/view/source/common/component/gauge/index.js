651184896971d3fc5ca1397ef1b1a19f069a7704
t1mer <deart1mer@gmail.com>
2016-05-14 20:41:04 +0800

    remove old

--- Web/view/source/common/component/gauge/index.js
+++
@@ -1,6 +0,0 @@
-import './gaugeStyle.css';
-import GaugeDirective from './gaugeDirective';
-
-export default angular
-    .module('imMap.Gauge', [])
-    .directive('imMapGauge', () => new GaugeDirective);
