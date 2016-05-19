e905a7948fa32e355aa5102b5c4ac7bb7220270c
t1mer <deart1mer@gmail.com>
2016-05-19 23:44:42 +0800

    api basemonitor

--- Web/source/monitor/service/GetBaseMonitorListFactory.js
+++ Web/source/monitor/service/GetBaseMonitorListFactory.js
@@ -1,3 +1,15 @@
-/**
- * Created by t1mer on 16/5/19.
- */
+export function GetBaseMonitorListFactory($http, $q){
+  return (reqData)=>$http({
+    url: '/basemonitor/list',
+    method: 'GET',
+    data: reqData
+  }).then((res)=>{
+    let data = res.data;
+    if(data.errno == 0){
+      return $q.resolve(data.data.list)
+    }
+  }).catch((err)=>{
+
+  })
+}
+GetBaseMonitorListFactory.$inject = ['$http', '$q'];
